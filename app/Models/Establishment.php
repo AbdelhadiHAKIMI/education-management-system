172fe19581766470c741ae087e9c57e11af1f85c
Abdelhadi HAKIMI <hakimi.abdelhadi@univ-guelma.dz>
2025-05-12 15:53:00 +0100

    Add establishments and modifying its table

---
+++ app/Models/Establishment.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Establishment extends Model
+{
+    protected $fillable = [
+        'name',
+        'location',
+        'wilaya',
+        'phone',
+        'email',
+        'logo',
+        'registration_code',
+        'is_active',
+        'created_by'
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+
+    public $timestamps = false;
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+}
